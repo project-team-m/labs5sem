21c8385aa0f9b0806df5108e0e2c3acdf6fb0c2e
paket <paket@paket.ru>
2019-11-08 19:40:58 +0300

    lab2++ created

--- ivan/OOP/lub2/lub2++/main.cpp
+++ ivan/OOP/lub2/lub2++/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+using namespace std;
+
+class Student {
+
+public:
+    Student() {
+        cout << "Constructor created" << endl;
+    }
+
+    Student(Student &) {
+        cout << "Constructor copy" << endl;
+    }
+
+
+    ~Student() {
+        cout << "DeConstructor" << endl;
+    }
+};
+
+Student *test(Student &s) {
+    return &s;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Student *a = new Student();
+    test(*a);
     return 0;
 }
