033270b983ae334ba23b06d2a68fb919bae1c29d
dekeyel_laptop <dekeyel_laptop@gmail.com>
2019-11-09 13:05:22 +0300

    add 3.2 oop

--- dima/oop/lab3_j/src/com/company/Main.java
+++ dima/oop/lab3_j/src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
 	 Singleton a = Singleton.getInstance();
 	 a.check();
 	 Singleton b = Singleton.getInstance();
@@ -19,5 +19,16 @@ public class Main {
 	 System.out.println(aa);
 	 Singleton2 bb = Singleton2.getInstance();
 	 System.out.println(bb);
+    }*/
+
+    public static void main(String[] args) {
+        class2 a = class2.create();
+        class2.check();
+        class2 b = class2.create();
+        class2.check();
+        a = a.del();
+        class2.check();
+        b = b.del();
+        class2.check();
     }
 }
