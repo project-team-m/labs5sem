ce3c4a8d34f9f7d8d2931e43e3839defdcfcae0a
dekeyel <dekeyel@dekeyel.ru>
2019-12-04 14:39:05 +0300

    repear

--- dima/coding/Hemming_dop/Form1.Designer.cs
+++ dima/coding/Hemming_dop/Form1.Designer.cs
@@ -45,7 +45,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "D=>H";
+            this.textBox1.Text = "ABCD";
             // 
             // button1
             // 
