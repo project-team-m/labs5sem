6c6f38f376450f49e5c1bc20bd67814c87893ecc
dekeyel <dekeyel@dekeyel.ru>
2019-12-18 18:04:14 +0300

    add lab6 oop

---
+++ dima/a.txt
@@ -0,0 +1,260 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+char* area;
+const int n=32;
+const int size_bl = 16;
+//запись в файл
+void writeFile()
+{
+    ofstream file("hdd.txt",ios::out);
+    for(int i=0;i<n;i++)
+    {
+        file << "| "<<area[i]<<" ";
+    }
+    file.close();
+}
+//чтение из файла
+void readFile()
+{
+    ifstream file("hdd.txt",ios::in);
+    for(int i=0;i<n;i++)
+        file>>area[i];
+    file.close();
+}
+//начальная инициализация блока
+void init()
+{
+    for(int i=0;i<n;i++)
+        area[i]='=';
+}
+//проверка блока
+int CheckBlock(int countBlock)
+{
+    int i,j;
+    for (i=0; i<n-countBlock; i++)
+    {
+        if (area[i] == '=')
+        {
+            for (j = i; j < countBlock + i; j++)
+            {
+                if (area[j] == '=')
+                    continue;
+                else
+                    break;
+            }
+            if (j - i == countBlock )
+                return i;
+        }
+    }
+    return -1;
+}
+//очистка блока
+void freeBlock(int StartBlock, int blockCount)
+{
+    if (area[StartBlock] == '=') cout << "Невозможно освободить свободные блоки" << endl;
+    else{
+        for (int i = StartBlock; i<blockCount; i++)
+        {
+            area[i]='=';
+        }
+        cout << "Освобождено" << endl;
+    }
+}
+
+
+//запись в блок
+void setBlock(int start, int count)
+{
+    char value;
+    cout<<"Введите имя записи"<<endl;
+    cin>>value;
+    for (int i=0;i<count;i++)
+    {
+        area[start++]=value;
+    }
+}
+//чтение из блока
+void readBlock(int start,int count)
+{
+    for (int i=0;i<count;i++)
+        cout<<area[start++];
+}
+//показать сожержимое блока
+void printBlock()
+{
+    for(int i=0;i<n;i++)
+        cout<<"| "<<area[i]<<" ";
+}
+
+int findBlock(char ch){
+    int num=-1;
+    for (int i = 0; i < n;)
+    {
+        if (area[i] == ch) {
+            num = i; break;
+        }
+        else i++;
+    }
+    return num;
+}
+
+int colDelBlock(char ch){
+    int num = 0;
+    for (int i = 0; i < n;)
+    {
+        if (area[i] == ch) {
+            num ++;
+            i++;
+        }
+        else i++;
+    }
+    return num;
+}
+
+int colEemp(){
+    int col = 0;
+    for (int i = 0; i < n; )
+    {
+        if (area[i] == '=') {
+            col++; i++;
+        }
+        else i++;
+    }
+    return col;
+}
+
+int colFull(){
+    int col = 0;
+    for (int i = 0; i < n; )
+    {
+        if (area[i]!= '=') {
+            col++; i++;
+        }
+        else i++;
+    }
+    return col;
+}
+
+void printInfo(){
+    cout << "Размер блока " << size_bl << endl;
+    cout << "Общее количество блоков " << n << endl;
+    cout << "Количество пустых блоков " << colEemp() << endl;
+    cout << "Количество занятых блоков " << colFull() << endl;
+}
+
+int main()
+{
+
+    setlocale(LC_ALL,"Russian");
+    area = new char[n];
+    init();
+    writeFile();
+    int key = 0;
+    while(true)
+    {
+        cout<<endl;
+        cout<<"Выберите действие"<<endl;
+        cout<<"1-выделить блоки и записать в них данные"<<endl;
+        cout<<"2-освобождение блоков"<<endl;
+        cout<<"3-чтение данных из блоков "<<endl;
+        cout<<"4-просмотр организации блоков"<<endl;
+        cout<<"5-инициализация"<<endl;
+        cout << "6-информации о состоянии блочного пространства" << endl;
+        cout << "7-завершение транзакции" << endl;
+        cout << "8-откат транзакции" << endl;
+        try
+        {
+            key = getchar() - 48;
+            cout << key << endl;
+        }
+        catch (const std::exception&)
+        {
+            cout << "Введены неверные значения" << endl;
+        }
+        switch (key)
+        {
+            case 1:
+                int count_block, number_block, size_inf;
+                cout<<"Размер информации для записи в файл"<<endl;
+                cin >> size_inf;
+                if (size_inf % size_bl != 0){
+                    count_block = ceil(size_inf / size_bl+0.5);
+                }
+                else count_block = size_inf / size_bl;
+                number_block = CheckBlock(count_block);
+                if (number_block == -1)
+                {
+                    cout<<"Данное количество блоков невозможно захватить"<<endl;
+                    
+                    break;
+                }
+                else
+                {
+                    cout<<count_block<<" блоков захвачено начиная с позиции "<<number_block<<endl;
+                    setBlock(number_block,count_block);
+                }
+                
+                break;
+            case 2:
+                int start_block,
+                        countblock;
+                char name_rec;
+                cout<<"какую запись необходимо удалить?"<<endl;
+                cin >> name_rec;
+                start_block = findBlock(name_rec);
+                countblock = colDelBlock(name_rec) + start_block;
+                freeBlock(start_block,countblock);
+                
+                break;
+
+            case 3: 
+                int st,	cnt;
+                cout<<"Сколько читать?"<<endl;
+                cin>>cnt;
+                cout<<"Откуда?"<<endl;
+                cin>>st;
+                readBlock(st,cnt);
+                
+                break;
+            case 4:
+                cout << endl;
+                printBlock();
+                cout << endl;
+                cout << endl;
+
+                
+                break;
+            case 5:
+                init();
+                cout<<"Инициализированно"<<endl;
+                
+                break;
+            case 6:
+                printInfo();
+                cout << endl;
+                cout << endl;
+                
+                break;
+            case 7:
+                writeFile();
+                cout << endl;
+                cout << endl;
+                
+                break;
+            case 8:
+                init();
+                writeFile();
+                cout << endl;
+                cout << endl;
+                
+                break;
+        }
+    }
+
+    
+    return 0;
+}
---
+++ dima/oop/lab6/first.cpp
@@ -0,0 +1,359 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class List {
+public:
+    class nodelist {
+    public:
+        int key;
+        int data;
+        nodelist* prev = nullptr;
+        nodelist* next = nullptr;
+    };
+    nodelist* first;
+    bool isEmpty() {
+
+        if (first == nullptr) return true;
+
+        else return false;
+    }
+    int count() {
+
+        int count = 1;
+
+        if (isEmpty())	count = -1;
+
+        nodelist* tmp = first;
+
+        while (tmp != nullptr)
+        {
+            count++;
+            tmp = tmp->next;
+        }
+        return count;
+    }
+    void del() {
+
+        nodelist* tmp = first;
+
+        while (first)
+        {
+            tmp = first;
+            first = first->next;
+            delete[] tmp;
+        }
+    }
+    int findData(int key)
+    {
+
+        if (isEmpty()) {
+
+            return NULL;
+        }
+        nodelist* tmp = findNode(key);
+
+        if (tmp != nullptr)return tmp->data;
+
+        return NULL;
+    }
+    void pop(int key) {
+
+        if (isEmpty())
+        {
+
+            cout << "List is empty" << endl;
+
+            return;
+        }
+
+        nodelist* tmp = findNode(key);
+
+        if (tmp == nullptr)
+        {
+            cout << "Element out of list  " << endl;
+            return;
+        }
+        if (tmp->next == nullptr && tmp->prev != nullptr)
+        {
+            nodelist* help = tmp->prev;
+            help->next = nullptr;
+            delete tmp;
+            cout << "Deleted" << endl;
+        }
+        else if (tmp == first)
+        {
+            if (tmp->next == nullptr)
+            {
+                first = nullptr;
+                delete tmp;
+                cout << " Deleted " << endl;
+            }
+            else
+            {
+                nodelist* help = tmp->next;
+                help->prev = nullptr;
+                first = help;
+                delete tmp;
+                cout << "Deleted " << endl;
+                while (help)
+                {
+                    help->key--;
+                    help = help->next;
+                }
+            }
+        }
+        else
+        {
+            nodelist* help_1 = tmp->prev;
+            nodelist* help_2 = tmp->next;
+            help_1->next = help_2;
+            help_2->prev = help_1;
+
+            delete tmp;
+            cout << "Deleted " << endl;
+
+            while (help_2)
+            {
+                help_2->key--;
+                help_2 = help_2->next;
+            }
+        }
+    }
+    void push(int data) {
+
+        if (isEmpty())
+        {
+            nodelist* tmp = new nodelist();
+            tmp->key = 1;
+            tmp->data = data;
+            tmp->next = nullptr;
+            tmp->prev = nullptr;
+            first = tmp;
+
+        }
+        else
+        {
+            nodelist* help = first;
+
+            while (help->next)  help = help->next;
+
+            nodelist* tmp = new nodelist();
+
+            tmp->key = count();
+            tmp->data = data;
+            tmp->next = nullptr;
+            tmp->prev = help;
+            help->next = tmp;
+
+        }
+    }
+    void show() {
+
+        if (isEmpty()) {
+            cout << "List is empty" << endl;
+            return;
+        }
+
+        nodelist* tmp = first;
+        while (tmp != nullptr) {
+            cout << tmp->data << endl;
+            tmp = tmp->next;
+        }
+    }
+private:
+    void addBefore(int key, int data) {
+
+        nodelist* ptr = findNode(key);
+
+        if (!ptr) cout << "Key not found " << endl;
+
+        else if (!ptr->prev) {
+            nodelist* tmp = new nodelist;
+            tmp->data = data;
+            tmp->key = ptr->key;
+
+            while (ptr)
+            {
+                ptr->key = ptr->key + 1;
+                ptr = ptr->next;
+            }
+            tmp->next = first;
+            first = tmp;
+        }
+
+        else {
+            nodelist* f = first;
+            while (f->next != ptr) f = f->next;
+            nodelist* tmp = new nodelist;
+            tmp->data = data;
+            tmp->key = ptr->key;
+
+            tmp->next = ptr;
+            tmp->prev = f;
+            f->next = tmp;
+            while (ptr) {
+                ptr->key = ptr->key + 1;
+                ptr = ptr->next;
+            }
+        }
+    }
+    nodelist* findNode(int key)
+    {
+        nodelist* tmp = first;
+        while (tmp->key != key && tmp->next != nullptr)
+        {
+            tmp = tmp->next;
+        }
+        if (tmp->next == nullptr && tmp->key != key) return nullptr;
+        return tmp;
+    }
+};
+
+class Queue1
+{
+    List objList;
+
+public:
+    void show()
+    {
+        objList.show();
+    }
+    void push(int data)
+    {
+        objList.push(data);
+    }
+    int pop() {
+        int n = objList.findData(1);
+        objList.pop(1);
+        return n;
+    }
+};
+
+class Queue2 : public List
+{
+public:
+    using List::isEmpty;
+    using List::push;
+    using List::show;
+    int pop()
+    {
+        int n;
+        nodelist* tmp = first;
+        n = tmp->data;
+        first = tmp->next;
+        first->prev = nullptr;
+        delete tmp;
+        return n;
+    }
+};
+
+class Menu
+{
+public:
+    vector <string> menu_item;
+
+    Menu()
+    {
+        menu_item.push_back("Show");
+        menu_item.push_back("Add elem");
+        menu_item.push_back("Del elem");
+        menu_item.push_back("Exit");
+    }
+
+    void ProcessMenu() {
+        while (true)
+        {
+            for (int i = 0; i < menu_item.size(); i++)
+            {
+                cout << i + 1 << ". " << menu_item.at(i).c_str() << endl;
+            }
+
+            int number;
+            cin >> number;
+            if (number == menu_item.size()) return;
+            if (number > menu_item.size() || number <= 0) continue;
+            ProcessMenuItem(number);
+        }
+    }
+
+    int AddMenuItem(std::string name)
+    {
+        menu_item.back() = name;
+        menu_item.push_back("Exit");
+        return menu_item.size() - 1;
+    }
+    virtual void ProcessMenuItem(int number) = 0;
+};
+
+class ListMenu : public Menu
+{
+    List objList;
+public:
+    void ProcessMenuItem(int number) {
+        system("cls");
+        switch (number) {
+
+            case 1:
+                objList.show();
+                break;
+            case 2:
+                int data;
+                cout << "Enter data: " << endl;
+                cin >> data;
+                objList.push(data);
+                break;
+            case 3:
+                int num;
+                cout << "Enter number element: " << endl;
+                cin >> num;
+                objList.pop(num);
+                break;
+        }
+    }
+};
+
+class QueueMenu : public Menu
+{
+    Queue1 objQueue;
+public:
+    void ProcessMenuItem(int number) {
+        system("cls");
+        switch (number) {
+            case 1:
+                objQueue.show();
+                break;
+            case 2:
+                int data;
+                cout << "Enter data: " << endl;
+                cin >> data;
+                objQueue.push(data);
+                break;
+
+            case 3:
+                cout << objQueue.pop() << endl;
+                break;
+
+
+            case 0:
+                return;
+        }
+    }
+
+};
+
+int main3()
+{
+    setlocale(LC_ALL, "rus");
+
+    ListMenu M;
+    M.ProcessMenu();
+
+    //QueueMenu m;
+    //m.ProcessMenu();
+
+    system("pause");
+    return 0;
+}
